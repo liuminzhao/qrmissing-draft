79936b7806846f2fd55efc66bd7df8081d2de21d
Minzhao Liu <liuminzhao@gmail.com>
2013-07-02 16:04:01 -0500

    new simulation on t error

--- code/sim-t-error.R
+++ code/sim-t-error.R
@@ -1,9 +1,10 @@
 #!/bin/Rscript
-##' Time-stamp: <liuminzhao 06/27/2013 15:19:44>
+##' Time-stamp: <liuminzhao 07/02/2013 16:03:30>
 ##' Simulation for paper,
 ##' T error
 ##' 2013/06/24
 ##' MAR
+##' 2013/07/02 new t3 error
 
 rm(list = ls())
 source('BiMLESigma.R')
@@ -13,20 +14,21 @@ library(xtable)
 library(doMC)
 registerDoMC()
 options(cores = 8)
-set.seed(1)
-
-
+set.seed(3)
 
 ###############
 ## PARAMETER
 ###############
 n <- 500
 p <- 0.5
+alpha <- 0.3
 
 ###############
 ## SIMULATION
 ###############
 boot <- 8
+
+time <- proc.time()[3]
 
 result <- foreach(icount(boot), .combine = rbind) %dopar% {
   R <- rbinom(n, 1, p)
@@ -37,14 +39,13 @@ result <- foreach(icount(boot), .combine = rbind) %dopar% {
   w <- rgamma(n, v/2, rate = v/2)
   winv <- 1/w
 
-
   for (i in 1:n){
     if (R[i] == 1){
-      y[i, 1] <- rnorm(1, 1 + x[i], sd = sqrt(winv[i]))
-      y[i, 2] <- rnorm(1, 1 + x[i] + y[i, 1]*1/2, sd = sqrt(3*winv[i]/4))
+      y[i, 1] <-  12 + x[i] + (1 + alpha*x[i])*rnorm(1, 0, sd = sqrt(winv[i]))
+      y[i, 2] <-  y[i, 1]*1/2  +  (1 + alpha*x[i])*rnorm(1, 0, sd = sqrt(3*winv[i]/4))
     } else {
-      y[i, 1] <- rnorm(1, -1 - x[i], sd = sqrt(winv[i]))
-      y[i, 2] <- rnorm(1, 1 + x[i] + y[i, 1]/2, sd = sqrt(3*winv[i]/4))
+      y[i, 1] <-  -12 - x[i] +(1 + alpha*x[i])*rnorm(1, 0, sd = sqrt(winv[i]))
+      y[i, 2] <-  y[i, 1]*1/2  +  (1 + alpha*x[i])*rnorm(1, 0, sd = sqrt(3*winv[i]/4))
     }
   }
 
@@ -58,11 +59,11 @@ result <- foreach(icount(boot), .combine = rbind) %dopar% {
   mod7 <- BiQRGradient(y, R, X, tau = 0.7, method = 'heter2')
   mod9 <- BiQRGradient(y, R, X, tau = 0.9, method = 'heter2')
 
-  mod1mm <- mysummary(mod1)
-  mod3mm <- mysummary(mod3)
-  mod5mm <- mysummary(mod5)
-  mod7mm <- mysummary(mod7)
-  mod9mm <- mysummary(mod9)
+  mod1mm <- coef(mod1)
+  mod3mm <- coef(mod3)
+  mod5mm <- coef(mod5)
+  mod7mm <- coef(mod7)
+  mod9mm <- coef(mod9)
 
   mod1rq <- as.vector(rq(y[,1]~x, tau = c(0.1, 0.3, 0.5, 0.7, 0.9))$coef)
   mod2rq <- as.vector(rq(y[,2][R==1]~x[R==1], tau = c(0.1, 0.3, 0.5, 0.7, 0.9))$coef)
@@ -74,7 +75,6 @@ result <- foreach(icount(boot), .combine = rbind) %dopar% {
 write.table(result, file = "sim-t-error.txt", row.names = F, col.names = F)
 sendEmail(subject="simulation-t-MAR", text="done", address="liuminzhao@gmail.com")
 
-
 ###############
 ## AFTER SIM; GET SUMMARY
 ###############
@@ -83,47 +83,46 @@ sendEmail(subject="simulation-t-MAR", text="done", address="liuminzhao@gmail.com
 ###############
 
 quan1 <- function(y, x, tau){
-  return(tau - .5*pt(y - 1- x, df = 3) - .5*pt(y+1+x,df = 3))
+  return(tau - .5*pt((y - 12- x)/(1+alpha*x), df = 3) - .5*pt((y+12+x)/(1+alpha*x),df = 3))
 }
 
 SolveQuan1 <- function(x, tau){
   uniroot(quan1, c(-30, 30), x = x, tau = tau)$root
 }
 
-x <- seq(0, 2, len = 100)
-y15 <- sapply(x, function(x) SolveQuan1(x, 0.5))
-y19 <- sapply(x, function(x) SolveQuan1(x, 0.9))
-y17 <- sapply(x, function(x) SolveQuan1(x, 0.7))
-y13 <- sapply(x, function(x) SolveQuan1(x, 0.3))
-y11 <- sapply(x, function(x) SolveQuan1(x, 0.1))
+xsim <- seq(0, 2, len = 100)
+y11 <- sapply(xsim, function(x) SolveQuan1(x, 0.1))
+y13 <- sapply(xsim, function(x) SolveQuan1(x, 0.3))
+y15 <- sapply(xsim, function(x) SolveQuan1(x, 0.5))
+y17 <- sapply(xsim, function(x) SolveQuan1(x, 0.7))
+y19 <- sapply(xsim, function(x) SolveQuan1(x, 0.9))
 
-q11 <- lm(y11~x)$coef
-q13 <- lm(y13~x)$coef
-q15 <- lm(y15~x)$coef
-q17 <- lm(y17~x)$coef
-q19 <- lm(y19~x)$coef
+q11 <- lm(y11~xsim)$coef
+q13 <- lm(y13~xsim)$coef
+q15 <- lm(y15~xsim)$coef
+q17 <- lm(y17~xsim)$coef
+q19 <- lm(y19~xsim)$coef
 
 quan2 <- function(y, x, tau){
-  return(tau - .5*pt(y - 1.5- 1.5*x, df = 3) - .5*pt(y - 0.5 - 0.5*x,df = 3))
+  return(tau - .5*pt((y - 6- 0.5*x)/(1+alpha*x), df = 3) - .5*pt((y + 6 + 0.5*x)/(1 + alpha*x),df = 3))
 }
 
 SolveQuan2 <- function(x, tau){
   uniroot(quan2, c(-30, 30), x = x, tau = tau)$root
 }
 
-x <- seq(0, 2, len = 100)
-y25 <- sapply(x, function(x) SolveQuan2(x, 0.5))
-y29 <- sapply(x, function(x) SolveQuan2(x, 0.9))
-y27 <- sapply(x, function(x) SolveQuan2(x, 0.7))
-y23 <- sapply(x, function(x) SolveQuan2(x, 0.3))
-y21 <- sapply(x, function(x) SolveQuan2(x, 0.1))
+xsim <- seq(0, 2, len = 100)
+y25 <- sapply(xsim, function(x) SolveQuan2(x, 0.5))
+y29 <- sapply(xsim, function(x) SolveQuan2(x, 0.9))
+y27 <- sapply(xsim, function(x) SolveQuan2(x, 0.7))
+y23 <- sapply(xsim, function(x) SolveQuan2(x, 0.3))
+y21 <- sapply(xsim, function(x) SolveQuan2(x, 0.1))
 
-q21 <- lm(y21~x)$coef
-q23 <- lm(y23~x)$coef
-q25 <- lm(y25~x)$coef
-q27 <- lm(y27~x)$coef
-q29 <- lm(y29~x)$coef
-
+q21 <- lm(y21~xsim)$coef
+q23 <- lm(y23~xsim)$coef
+q25 <- lm(y25~xsim)$coef
+q27 <- lm(y27~xsim)$coef
+q29 <- lm(y29~xsim)$coef
 
 result <- read.table('sim-t-error.txt')
 trueq <- c(q11, q13, q15, q17, q19, q21, q23, q25, q27, q29)
@@ -135,7 +134,13 @@ for (i in 1:40){
 
 mseh2 <- rbind(matrix(mse[1:10], 2, 5), matrix(mse[11:20], 2, 5))
 mserq <- rbind(matrix(mse[21:30], 2, 5), matrix(mse[31:40], 2, 5))
-print(mseh2)
-print(mserq)
+mytbl <- cbind(mseh2[,1], mserq[,1], mseh2[,2], mserq[,2], mseh2[,3], mserq[,3], mseh2[,4], mserq[,4], mseh2[,5], mserq[,5])
+
 print(xtable(mseh2))
 print(xtable(mserq))
+
+colnames(mytbl) <- rep(c('MM', 'RQ'), 5)
+
+print(xtable(mytbl))
+
+cat("Time: ", proc.time()[3] - time, '\n')
