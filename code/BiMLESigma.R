3d5d1a72fa9b5f0d8bd55a466f3517a3d28230d5
Minzhao Liu <liuminzhao@gmail.com>
2013-07-02 15:45:33 -0500

    bivariate: add class and other generic functions

--- code/BiMLESigma.R
+++ code/BiMLESigma.R
@@ -1,4 +1,4 @@
-## Time-stamp: <liuminzhao 07/02/2013 13:25:14>
+## Time-stamp: <liuminzhao 07/02/2013 14:59:55>
 ## WRAP UP BiMLESigma.f
 
 BiQRGradient <- function(y, R, X, tau=0.5, niter = 1000, sp = rep(0, 1 + 2*dim(X)[2]), method = 'heter2'){
@@ -103,7 +103,52 @@ BiQRGradient <- function(y, R, X, tau=0.5, niter = 1000, sp = rep(0, 1 + 2*dim(X
                     converge = as.logical(TRUE)
                     )
   }
+  mod$method <- method
+  class(mod) <- "BiQRGradient"
   return(mod)
+}
+
+coef.BiQRGradient <- function(mod, ...){
+  q <- mod$xdim
+  param <- mod$param[1:(8*q)]
+  coef <- matrix(param, 8, q, byrow = T)
+  coef <- coef[c(1, 5, 2, 6, 3, 4, 7, 8), ]
+  p <- mod$param[8*q + 3]
+  beta22 <- mod$param[8*q + 1]
+  h <- mod$param[8*q + 2]
+  rownames(coef) <- c('Q1Coef', 'Q2Coef', 'beta1(0)', 'beta2(0)',
+                      'Sigma1(1)', 'Sigma1(0)', 'Sigma2(1)', 'Sigma2(0)')
+  return(coef[c(1, 2),])
+}
+
+print.BiQRGradient <- function(mod, ...){
+  cat('Coefficients: \n')
+  print(coef(mod))
+}
+
+summary.BiQRGradient <- function(mod, ...){
+  n <- mod$n
+  R <- mod$R
+  tau <- mod$tau
+  param <- mod$param
+  q <- mod$xdim
+
+  cat('Number of observations: ', n, '\n')
+  cat('Sample proportion of observed data: ', sum(R)/n, '\n')
+  cat('Estimated pi:', param[8*q + 3], '\n')
+  cat('Quantile: ', tau, '\n')
+  cat('Quantile regression coefficients: \n')
+  print(coef(mod))
+}
+
+plot.BiQRGradient <- function(mod, ...){
+  a <- matrix(mod$paramsave, mod$niter, length(mod$param) + 1)
+  xdim <- mod$xdim
+  colnames(a) <- c(paste('gamma1', 1:xdim, sep=''), paste('beta1', 1:xdim, sep=''), paste('sigma1', 1:xdim, sep = ''), paste('sigma0', 1:xdim, sep = ''), paste('gamma2', 1:xdim, sep=''), paste('beta1', 1:xdim, sep=''), paste('sigma2', 1:xdim, sep = ''), paste('sp', 1:xdim, sep = ''), 'beta22', 'h', 'p', 'nll')
+  par(mfrow = c(2, 2))
+  for (i in 1:dim(a)[2]){
+    plot(ts(a[, i]), main = colnames(a)[i])
+  }
 }
 
 Diagnose <- function(mod){
