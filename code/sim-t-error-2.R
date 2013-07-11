39444a1707c48369968dc6f29b6c574c3571cd3d
Minzhao Liu <liuminzhao@gmail.com>
2013-07-11 01:14:17 -0500

    keep alpha = 0.5 instead of 0.3, and do simulation for t error again , with MM, RQ, BB together

--- code/sim-t-error-2.R
+++ code/sim-t-error-2.R
@@ -1,16 +1,18 @@
 #!/bin/Rscript
-##' Time-stamp: <liuminzhao 07/08/2013 16:16:16>
+##' Time-stamp: <liuminzhao 07/11/2013 01:11:42>
 ##' Simulation for paper,
 ##' T error
 ##' 2013/06/24
 ##' MAR
 ##' 2013/07/02 new t3 error
 ##' 2013/07/07 smaller version of t error like normal
+##' 2013/07/11 keep alpha = 0.5 instead of 0.3 and do MM, RQ, BB together
 
-sink('0707-t.txt')
+sink('0711-t.txt')
 rm(list = ls())
 source('BiMLESigma.R')
 source('sendEmail.R')
+source('Bottai.R')
 library(quantreg)
 library(xtable)
 library(doMC)
@@ -23,13 +25,13 @@ set.seed(3)
 ###############
 n <- 500
 p <- 0.5
-alpha <- 0.3
+alpha <- 0.5
 
 ###############
 ## SIMULATION
 ###############
-boot <- 200
-count <- rep(0, 5)
+boot <- 1000
+
 start <- proc.time()[3]
 
 result <- foreach(icount(boot), .combine = rbind) %dopar% {
@@ -66,20 +68,25 @@ result <- foreach(icount(boot), .combine = rbind) %dopar% {
   mod5mm <- coef(mod5)
   mod7mm <- coef(mod7)
   mod9mm <- coef(mod9)
-  count[1] <- count[1] + mod1$converge
-  count[2] <- count[2] + mod3$converge
-  count[3] <- count[3] + mod5$converge
-  count[4] <- count[4] + mod7$converge
-  count[5] <- count[5] + mod9$converge
 
   mod1rq <- as.vector(rq(y[,1]~x, tau = c(0.1, 0.3, 0.5, 0.7, 0.9))$coef)
   mod2rq <- as.vector(rq(y[,2][R==1]~x[R==1], tau = c(0.1, 0.3, 0.5, 0.7, 0.9))$coef)
 
+  mod1b <- Bottai(y, R, X, tau = 0.1)
+  mod3b <- Bottai(y, R, X, tau = 0.3)
+  mod5b <- Bottai(y, R, X, tau = 0.5)
+  mod7b <- Bottai(y, R, X, tau = 0.7)
+  mod9b <- Bottai(y, R, X, tau = 0.9)
+
   ans <- c(mod1mm[1,], mod3mm[1,], mod5mm[1,], mod7mm[1,], mod9mm[1,],
-           mod1mm[2,], mod3mm[2,], mod5mm[2,], mod7mm[2,], mod9mm[2,], mod1rq, mod2rq, count)
+           mod1mm[2,], mod3mm[2,], mod5mm[2,], mod7mm[2,], mod9mm[2,],
+           mod1rq, mod2rq,
+           mod1b[,1], mod3b[,1], mod5b[,1], mod7b[,1], mod9b[,1],
+           mod1b[,2], mod3b[,2], mod5b[,2], mod7b[,2], mod9b[,2])
+
 }
 
-write.table(result, file = "0707-t-result.txt", row.names = F, col.names = F)
+write.table(result, file = "0711-t-result.txt", row.names = F, col.names = F)
 sendEmail(subject="simulation-t-MAR", text="done", address="liuminzhao@gmail.com")
 
 ###############
@@ -131,47 +138,47 @@ q25 <- lm(y25~xsim)$coef
 q27 <- lm(y27~xsim)$coef
 q29 <- lm(y29~xsim)$coef
 
-result <- read.table('0707-t-result.txt')
+
+
+result <- read.table('0711-t-result.txt')
 trueq <- c(q11, q13, q15, q17, q19, q21, q23, q25, q27, q29)
-trueq <- rep(trueq, 2)
+trueq <- rep(trueq, 3)
 
 ## MSE
-mse <- rep(0, 40)
-for (i in 1:40){
+mse <- rep(0, 60)
+for (i in 1:60){
   mse[i] <- mean((result[,i] - trueq[i])^2, trim = 0.05)
 }
 mseh2 <- rbind(matrix(mse[1:10], 2, 5), matrix(mse[11:20], 2, 5))
 mserq <- rbind(matrix(mse[21:30], 2, 5), matrix(mse[31:40], 2, 5))
-mytbl <- cbind(mseh2[,1], mserq[,1], mseh2[,2], mserq[,2], mseh2[,3], mserq[,3], mseh2[,4], mserq[,4], mseh2[,5], mserq[,5])
-print(xtable(mseh2))
-print(xtable(mserq))
-colnames(mytbl) <- rep(c('MM', 'RQ'), 5)
+msebb <- rbind(matrix(mse[41:50], 2, 5), matrix(mse[51:60], 2, 5))
+mytbl <- cbind(mseh2[,1], mserq[,1], msebb[,1], mseh2[,2], mserq[,2], msebb[,2], mseh2[,3], mserq[,3], msebb[,3], mseh2[,4], mserq[,4], msebb[,4], mseh2[,5], mserq[,5], msebb[,5])
+colnames(mytbl) <- rep(c('MM', 'RQ', 'BB'), 5)
 print(xtable(mytbl))
+print(mytbl)
 
 ## BIAS
-bias <- rep(0, 40)
-for (i in 1:40){
-##  bias[i] <- mean((result[,i] - trueq[i])/abs(trueq[i]))
-##  bias[i] <- mean((result[,i] - trueq[i])/abs(trueq[i]), trim = 0.05)
+bias <- rep(0, 60)
+for (i in 1:60){
   bias[i] <- mean((result[,i] - trueq[i]), trim = 0.05)
 }
 biash2 <- rbind(matrix(bias[1:10], 2, 5), matrix(bias[11:20], 2, 5))
 biasrq <- rbind(matrix(bias[21:30], 2, 5), matrix(bias[31:40], 2, 5))
-mytbl2 <- cbind(biash2[,1], biasrq[,1], biash2[,2], biasrq[,2], biash2[,3], biasrq[,3], biash2[,4], biasrq[,4], biash2[,5], biasrq[,5])
-colnames(mytbl2) <- rep(c('MM', 'RQ'), 5)
+biasbb <- rbind(matrix(bias[41:50], 2, 5), matrix(bias[51:60], 2, 5))
+mytbl2 <- cbind(biash2[,1], biasrq[,1], biasbb[,1], biash2[,2], biasrq[,2], biasbb[,2], biash2[,3], biasrq[,3], biasbb[,3], biash2[,4], biasrq[,4], biasbb[,4], biash2[,5], biasrq[,5], biasbb[,5])
+colnames(mytbl2) <- rep(c('MM', 'RQ', 'BB'), 5)
 print(xtable(mytbl2))
+print(mytbl2)
 
-## EFFICIENCY
-efficiency <- apply(result[,1:40], 2, var)
+## Efficiency
+efficiency <- apply(result[,1:60], 2, var)
 efficiencyh2 <- rbind(matrix(efficiency[1:10], 2, 5), matrix(efficiency[11:20], 2, 5))
 efficiencyrq <- rbind(matrix(efficiency[21:30], 2, 5), matrix(efficiency[31:40], 2, 5))
-mytbl3 <- efficiencyrq/efficiencyh2
-colnames(mytbl3) <- seq(0.1, 0.9, length = 5)
+efficiencybb <- rbind(matrix(efficiency[41:50], 2, 5), matrix(efficiency[51:60], 2, 5))
+mytbl3 <- cbind(efficiencyrq/efficiencyh2, efficiencybb/efficiencyh2)
+colnames(mytbl3) <- rep(c('RQ', 'BB'), 5)
 print(xtable(mytbl3))
 
 cat("Time: ", proc.time()[3] - start, '\n')
-cat("Converged: ")
-count <- result[, 41:45]
-print(apply(count, 2, sum))
 
 sink()
