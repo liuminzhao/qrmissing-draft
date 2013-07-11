6f590587d62d7adf63c05afc38d3a393fc98b4bc
Minzhao Liu <liuminzhao@gmail.com>
2013-07-11 01:13:25 -0500

    add laplace distribution simulation , BIAS and EFFICIENCY comparison

--- code/sim-lp-error-2.R
+++ code/sim-lp-error-2.R
@@ -133,22 +133,46 @@ q25 <- lm(y25~xsim)$coef
 q27 <- lm(y27~xsim)$coef
 q29 <- lm(y29~xsim)$coef
 
+
 result <- read.table('sim-lp-error-0710.txt')
 trueq <- c(q11, q13, q15, q17, q19, q21, q23, q25, q27, q29)
 trueq <- rep(trueq, 3)
+
+## MSE
 mse <- rep(0, 60)
 for (i in 1:60){
   mse[i] <- mean((result[,i] - trueq[i])^2, trim = 0.05)
 }
-
 mseh2 <- rbind(matrix(mse[1:10], 2, 5), matrix(mse[11:20], 2, 5))
 mserq <- rbind(matrix(mse[21:30], 2, 5), matrix(mse[31:40], 2, 5))
 msebb <- rbind(matrix(mse[41:50], 2, 5), matrix(mse[51:60], 2, 5))
 mytbl <- cbind(mseh2[,1], mserq[,1], msebb[,1], mseh2[,2], mserq[,2], msebb[,2], mseh2[,3], mserq[,3], msebb[,3], mseh2[,4], mserq[,4], msebb[,4], mseh2[,5], mserq[,5], msebb[,5])
-
-colnames(mytbl) <- rep(c('MM', 'RQ', 'Bottai'), 5)
-
+colnames(mytbl) <- rep(c('MM', 'RQ', 'BB'), 5)
 print(xtable(mytbl))
 print(mytbl)
+
+## BIAS
+bias <- rep(0, 60)
+for (i in 1:60){
+  bias[i] <- mean((result[,i] - trueq[i]), trim = 0.05)
+}
+biash2 <- rbind(matrix(bias[1:10], 2, 5), matrix(bias[11:20], 2, 5))
+biasrq <- rbind(matrix(bias[21:30], 2, 5), matrix(bias[31:40], 2, 5))
+biasbb <- rbind(matrix(bias[41:50], 2, 5), matrix(bias[51:60], 2, 5))
+mytbl2 <- cbind(biash2[,1], biasrq[,1], biasbb[,1], biash2[,2], biasrq[,2], biasbb[,2], biash2[,3], biasrq[,3], biasbb[,3], biash2[,4], biasrq[,4], biasbb[,4], biash2[,5], biasrq[,5], biasbb[,5])
+colnames(mytbl2) <- rep(c('MM', 'RQ', 'BB'), 5)
+print(xtable(mytbl2))
+print(mytbl2)
+
+## Efficiency
+efficiency <- apply(result[,1:60], 2, var)
+efficiencyh2 <- rbind(matrix(efficiency[1:10], 2, 5), matrix(efficiency[11:20], 2, 5))
+efficiencyrq <- rbind(matrix(efficiency[21:30], 2, 5), matrix(efficiency[31:40], 2, 5))
+efficiencybb <- rbind(matrix(efficiency[41:50], 2, 5), matrix(efficiency[51:60], 2, 5))
+mytbl3 <- cbind(efficiencyrq/efficiencyh2, efficiencybb/efficiencyh2)
+colnames(mytbl3) <- rep(c('RQ', 'BB'), 5)
+print(xtable(mytbl3))
+
+
 cat("Time: ", proc.time()[3] - start, '\n')
 sink()
