37cea48f0cd3022de62330773ac1e91aabe93b51
Minzhao Liu <liuminzhao@gmail.com>
2013-07-14 20:12:08 -0500

    tours mnar bootstrap behave weird on y1 0.1 and y2 0.9, add track converge and re-fit those two models again

---
+++ tours/toursbootbasemnar2.R
@@ -0,0 +1,79 @@
+#!/bin/Rscript
+##' Time-stamp: <liuminzhao 07/14/2013 20:11:01>
+##' bootstrap on tours data
+##' weight2 and weight3
+##' scaled by 1/100
+##' covariates: 3 treatments and 2 races
+##' 2013/07/07 MNAR
+##' 2013/07/14 y1 0.1 and y2 0.9 bootstrap CI behave weird,
+##' not include estimated statistics, thus track converge too.
+##' and only fit mod1 and mod9
+
+rm(list=ls())
+source('~/Documents/qrmissing/code/ToursMNAR.R')
+source('~/Documents/qrmissing/code/sendEmail.R')
+library(doMC)
+registerDoMC()
+options(cores = 8)
+set.seed(1)
+
+TOURS <- read.csv('~/Documents/qrmissing/tours/tours.csv')
+TOURS <- subset(TOURS, RACE==1 | RACE==3)
+weight1 <- TOURS$wtkg1
+weight2 <- TOURS$wtkg2
+weight3 <- TOURS$wtkg3
+age <- TOURS$AGE
+trt <- TOURS$TREATMENT
+race3 <- as.numeric(TOURS$RACE == 3)
+age_center <- (age-mean(age))/sd(age)
+
+## scaled by 1/100
+weight1 <- weight1/100
+weight2 <- weight2/100
+weight3 <- weight3/100
+
+## new dataset
+NEWTOURS <- data.frame(w2 = weight2, w3 = weight3, age_center, race3, w1 = weight1)
+
+###############
+## BOOTSTRAP
+###############
+boot <- 1000
+n <- dim(NEWTOURS)[1]
+y <- matrix(0, n, 2)
+X <- matrix(0, n, 4)
+X[,1] <- 1
+
+start <- proc.time()[3]
+
+result <- foreach(icount(boot), .combine = rbind) %dopar% {
+  indices <- sample(n, replace = TRUE)
+  dat <- NEWTOURS[indices, ]
+  y[, 1] <- dat$w2
+  y[, 2] <- dat$w3
+  X[,2] <- dat$age_center
+  X[,3] <- dat$race3
+  X[,4] <- dat$w1
+  R <- 1 - as.numeric(is.na(dat$w3))
+  y[is.na(y[,2]),2] <- 0
+
+  mod1 <- ToursMNAR(y, R, X, tau = 0.05, method = 'heter2', niter = 2000)
+  mod9 <- ToursMNAR(y, R, X, tau = 0.95, method = 'heter2', niter = 2000)
+
+  coef1 <- coef(mod1)
+  coef9 <- coef(mod9)
+
+  count1 <- mod1$converge
+  count9 <- mod9$converge
+
+  coefw2 <- c(coef1[1,], coef9[1,])
+  coefw3 <- c(coef1[2,], coef9[2,])
+
+  ans <- c(coefw2, coefw3, count1, count9)
+
+}
+
+write.table(result, file = "toursbootagebbasemnar-19.txt", row.names = FALSE, col.names = FALSE)
+sendEmail(subject="boot tours base mnar 1-9", text="done", address="liuminzhao@gmail.com")
+
+print(proc.time()[3] - start)
