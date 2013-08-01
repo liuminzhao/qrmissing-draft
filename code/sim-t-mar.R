0f7bec219e505d9ea5717af555f20686e1d722ce
Minzhao Liu <liuminzhao@gmail.com>
2013-08-01 08:34:40 -0500

    1. chnge boot to 100 from 1000 2. add simulation t MAR scenario 1 on QRMissingBi

--- code/sim-t-mar.R
+++ code/sim-t-mar.R
@@ -1,16 +1,19 @@
 #!/bin/Rscript
-##' Time-stamp: <liuminzhao 08/01/2013 08:32:36>
-##' Simulation Bivariate case with MAR using heter2
-##' Real MAR , not MCAR
-##' correct heterogeneity parameters
-##' 2013/07/18 add Bottai's , Normal MAR , scenario 1
-##' 2013/07/31 using QRMissingBi.R
+##' Time-stamp: <liuminzhao 08/01/2013 08:33:42>
+##' Simulation for paper,
+##' T error
+##' 2013/06/24
+##' MAR
+##' 2013/07/02 new t3 error
+##' 2013/07/07 smaller version of t error like normal
+##' 2013/07/11 keep alpha = 0.5 instead of 0.3 and do MM, RQ, BB together
+##' 2013/08/01 test on QRMissingBi.R
 
-sink('sim-normal-mar-0731.txt')
+sink('sim-t-mar-0801.txt')
 rm(list = ls())
 library(compiler)
 enableJIT(3)
-source('QRMissingBi.R')
+source('BiMLESigma.R')
 source('sendEmail.R')
 source('Bottai.R')
 library(quantreg)
@@ -38,13 +41,18 @@ result <- foreach(icount(boot), .combine = rbind) %dopar% {
   R <- rbinom(n, 1, p)
   x <- runif(n, 0, 2)
   y <- matrix(0, n, 2)
+
+  v <- 3
+  w <- rgamma(n, v/2, rate = v/2)
+  winv <- 1/w
+
   for (i in 1:n){
     if (R[i] == 1){
-      y[i, 1] <- 2 + x[i] +(1 + 0.5*x[i])*rnorm(1)
-      y[i, 2] <- 1 - x[i] - 0.5 * y[i, 1] + (1+0.5*x[i])*rnorm(1)
-    } else {
-      y[i, 1] <- -2 - x[i] +(1 + 0.5*x[i])*rnorm(1)
-      y[i, 2] <- 1 - x[i] - 0.5 * y[i, 1] + (1+0.5*x[i])*rnorm(1)
+    y[i, 1] <-  2 + x[i] + (1 + alpha*x[i])*rnorm(1, 0, sd = sqrt(winv[i]))
+    y[i, 2] <-  1 - x[i] - y[i, 1]*1/2  +  (1 + alpha*x[i])*rnorm(1, 0, sd = sqrt(3*winv[i]/4))
+  } else {
+    y[i, 1] <-  -2 - x[i] +(1 + alpha*x[i])*rnorm(1, 0, sd = sqrt(winv[i]))
+    y[i, 2] <-  1 - x[i] - y[i, 1]*1/2  +  (1 + alpha*x[i])*rnorm(1, 0, sd = sqrt(3*winv[i]/4))
     }
   }
 
@@ -66,11 +74,13 @@ result <- foreach(icount(boot), .combine = rbind) %dopar% {
 
   mod1rq <- as.vector(rq(y[,1]~x, tau = c(0.1, 0.3, 0.5, 0.7, 0.9))$coef)
   mod2rq <- as.vector(rq(y[,2][R==1]~x[R==1], tau = c(0.1, 0.3, 0.5, 0.7, 0.9))$coef)
+
   mod1b <- Bottai(y, R, X, tau = 0.1)
   mod3b <- Bottai(y, R, X, tau = 0.3)
   mod5b <- Bottai(y, R, X, tau = 0.5)
   mod7b <- Bottai(y, R, X, tau = 0.7)
   mod9b <- Bottai(y, R, X, tau = 0.9)
+
   ans <- c(mod1mm[1,], mod3mm[1,], mod5mm[1,], mod7mm[1,], mod9mm[1,],
            mod1mm[2,], mod3mm[2,], mod5mm[2,], mod7mm[2,], mod9mm[2,],
            mod1rq, mod2rq,
@@ -79,26 +89,22 @@ result <- foreach(icount(boot), .combine = rbind) %dopar% {
 
 }
 
-write.table(result, file = "sim-normal-mar-0731-result.txt", row.names = F, col.names = F)
-sendEmail(subject="simulation-normal-MAR", text="done", address="liuminzhao@gmail.com")
+write.table(result, file = "sim-t-mar-0801-result.txt", row.names = F, col.names = F)
+sendEmail(subject="simulation-t-MAR", text="done", address="liuminzhao@gmail.com")
 
+###############
+## AFTER SIM; GET SUMMARY
+###############
 ###############
 ## TRUE VALUE
 ###############
+
 quan1 <- function(y, x, tau){
-  return(tau - .5*pnorm(y, 2+x, 1 + 0.5*x) - .5*pnorm(y, -2-x, 1 + 0.5*x))
+  return(tau - .5*pt((y - 2- x)/(1+alpha*x), df = 3) - .5*pt((y+2+x)/(1+alpha*x),df = 3))
 }
 
 SolveQuan1 <- function(x, tau){
   uniroot(quan1, c(-30, 30), x = x, tau = tau)$root
-}
-
-quan2 <- function(y, x, tau){
-  return(tau - .5*pnorm(y, -1.5*x, (1+0.5*x)*sqrt(5/4)) - .5*pnorm(y, 2-.5*x, (1+0.5*x)*sqrt(5/4)))
-}
-
-SolveQuan2 <- function(x, tau){
-  uniroot(quan2, c(-30, 30), x = x, tau = tau)$root
 }
 
 xsim <- seq(0, 2, len = 100)
@@ -114,6 +120,14 @@ q15 <- lm(y15~xsim)$coef
 q17 <- lm(y17~xsim)$coef
 q19 <- lm(y19~xsim)$coef
 
+quan2 <- function(y, x, tau){
+  return(tau - .5*pt((y  + 1.5*x)/(1+alpha*x), df = 3) - .5*pt((y - 2 + 0.5*x)/(1 + alpha*x),df = 3))
+}
+
+SolveQuan2 <- function(x, tau){
+  uniroot(quan2, c(-30, 30), x = x, tau = tau)$root
+}
+
 xsim <- seq(0, 2, len = 100)
 y25 <- sapply(xsim, function(x) SolveQuan2(x, 0.5))
 y29 <- sapply(xsim, function(x) SolveQuan2(x, 0.9))
@@ -127,7 +141,9 @@ q25 <- lm(y25~xsim)$coef
 q27 <- lm(y27~xsim)$coef
 q29 <- lm(y29~xsim)$coef
 
-result <- read.table('sim-normal-mar-0731-result.txt')
+
+
+result <- read.table('sim-t-mar-0801-result.txt')
 trueq <- c(q11, q13, q15, q17, q19, q21, q23, q25, q27, q29)
 trueq <- rep(trueq, 3)
 
@@ -167,4 +183,5 @@ colnames(mytbl3) <- rep(c('RQ', 'BB'), 5)
 print(xtable(mytbl3))
 
 cat("Time: ", proc.time()[3] - start, '\n')
+
 sink()
