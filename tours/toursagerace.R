2a6ba61346143001f5f25e6cdea2f3e44f9f7cbe
Minzhao Liu <liuminzhao@gmail.com>
2013-08-07 11:48:48 -0500

    modify toursagerace.R to run Homo QRMissingBi model using default uobyqa method to get MAR result for tau 10%, 30, 50, 70, 90% results

--- tours/toursagerace.R
+++ tours/toursagerace.R
@@ -1,8 +1,18 @@
 #!/bin/Rscript
-## Time-stamp: <liuminzhao 06/06/2013 13:29:17>
-## manipulate data TOURS
-## 2013/06/05 focus on AGE and RACE
+##' Time-stamp: <liuminzhao 08/07/2013 11:47:31>
+##' manipulate data TOURS
+##' 2013/06/05 focus on AGE and RACE
+##' 2013/08/02 using QRMissingBi
+
 rm(list=ls())
+library(compiler)
+enableJIT(3)
+enableJIT(3)
+library(rootSolve)
+library(quantreg)
+library(minqa)
+source('../code/QRMissingBi.R')
+
 TOURS <- read.csv('~/Documents/qrmissing/tours/tours.csv')
 
 TOURS <- subset(TOURS, RACE==1 | RACE==3)
@@ -18,6 +28,7 @@ race3 <- as.numeric(TOURS$RACE == 3)
 ## center weight2?
 ## weight2 <- scale(weight2)
 ## weight3 <- scale(weight3)
+weight1 <- weight1/100
 weight2 <- weight2/100
 weight3 <- weight3/100
 
@@ -35,46 +46,28 @@ X[,3] <- race3
 
 R <- 1 - as.numeric(is.na(weight3))
 
-dat <- data.frame(weight2, weight3, trt, age_center, age=TOURS$AGE, race = factor(TOURS$RACE))
-
-## PLOT
-library(ggplot2)
-library(gridExtra)
-ggplot(data = dat, aes(x = age_center, y = weight2)) + geom_point()
-ggplot(data = dat, aes(x = age_center, y = weight3)) + geom_point()
-
-box1 <- ggplot(data = dat, aes(x = age, y = weight2*100)) + geom_point()+ ylab('Weight (Kg) after 6 months') + xlab('Age')
-box2 <- ggplot(data = dat, aes(x = age, y = weight3*100)) + geom_point()+ ylab('Weight (Kg) after 18 months') + xlab('Age')
-box3 <- ggplot(data = dat, aes(x = race, y = weight2*100)) + geom_boxplot() + scale_x_discrete(labels=c("Black", "White")) + ylab('Weight (Kg) after 6 months') + xlab('Race')
-box4 <- ggplot(data = dat, aes(race, y = weight3*100)) + geom_boxplot() + ylab('Weight (Kg) after 18 months') + xlab('Race') + scale_x_discrete(labels=c("Black", "White"))
-## scat1 <- ggplot(data = dat, aes(x = age_center, y = weight2, color = race)) + geom_point()
-## scat2 <- ggplot(data = dat, aes(x = age_center, y = weight3, color = race)) + geom_point()
-## scat3 <- ggplot(data = dat, aes(x = age_center, y = weight2, color = race)) + geom_point()
-## scat4 <- ggplot(data = dat, aes(x = age_center, y = weight3, color = race)) + geom_point()
-
-pdf('weight-plot-age.pdf')
-sds <- grid.arrange(box1, box2, box3, box4, nrow = 2, ncol = 2)
-dev.off()
-
+dat <- data.frame(weight2, weight3, trt, age_center, age=TOURS$AGE, race = factor(TOURS$RACE), base = weight1)
 
 ###############
 ## ANALYSIS
 ###############
 
-y[is.na(y[,2]),2] <- 0
+## y[is.na(y[,2]),2] <- 0
+QRMissingBic <- cmpfun(QRMissingBi)
 
-source('~/Documents/qrmissing/code/BiMLESigma.R')
-mod1 <- BiQRGradient(y, R, X, tau = 0.05, method = 'heter2')
-mod3 <- BiQRGradient(y, R, X, tau = 0.3, method = 'heter2')
-mod5 <- BiQRGradient(y, R, X, tau = 0.5, method = 'heter2')
-mod7 <- BiQRGradient(y, R, X, tau = 0.7, method = 'heter2')
-mod9 <- BiQRGradient(y, R, X, tau = 0.95, method = 'heter2')
+start <- proc.time()[3]
 
-coef1 <- mysummary(mod1)
-coef3 <- mysummary(mod3)
-coef5 <- mysummary(mod5)
-coef7 <- mysummary(mod7)
-coef9 <- mysummary(mod9)
+mod1 <- QRMissingBic(y, R, X, tau = 0.1)
+mod3 <- QRMissingBic(y, R, X, tau = 0.3)
+mod5 <- QRMissingBic(y, R, X, tau = 0.5)
+mod7 <- QRMissingBic(y, R, X, tau = 0.7)
+mod9 <- QRMissingBic(y, R, X, tau = 0.9)
+
+coef1 <- coef(mod1)
+coef3 <- coef(mod3)
+coef5 <- coef(mod5)
+coef7 <- coef(mod7)
+coef9 <- coef(mod9)
 
 coefw2 <- rbind(coef1[1,], coef3[1, ], coef5[1, ], coef7[1, ], coef9[1,])
 coefw3 <- rbind(coef1[2,], coef3[2, ], coef5[2, ], coef7[2, ], coef9[2,])
@@ -89,24 +82,6 @@ library(xtable)
 print(xtable(coefw2))
 print(xtable(coefw3))
 
-write.table(rbind(coefw2, coefw3), 'ageresult.txt', row.names=FALSE)
+cat("time: ", proc.time()[3] - start, '\n')
 
-####################
-
-## modrq = rq(change1 ~ age_center + factor(race), tau = seq(0.1,0.9,len =5))
-
-## modrq2 = rq(change2[R==1] ~ age_center[R==1] + factor(race)[R==1], tau = seq(0.1,0.9,len =5))
-
-## ## RESULTS
-
-## result1 <- cbind(mod1$param[c(1:3, 13:15)], mod3$param[c(1:3, 13:15)],
-##                  mod5$param[c(1:3, 13:15)], mod7$param[c(1:3, 13:15)],
-##                  mod9$param[c(1:3, 13:15)])
-
-## resultrq <- rbind(modrq$coef, modrq2$coef)
-
-## library(xtable)
-## print(xtable(result1))
-## print(xtable(resultrq))
-## write.table(result1, '../tours/result1.txt')
-## write.table(resultrq, '../tours/resultrq.txt')
+write.table(rbind(coefw2, coefw3), '../tours/agerace.txt', row.names=FALSE)
