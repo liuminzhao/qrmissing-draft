30aeb2acedaeee935f5014ec4f31be5587608ed3
Minzhao Liu <liuminzhao@gmail.com>
2013-08-08 23:14:09 -0500

    tours age race, bootstrap , MAR and MNAR3

--- tours/toursbootageracemnar.R
+++ tours/toursbootageracemnar.R
@@ -1,12 +1,16 @@
 #!/bin/Rscript
-## Time-stamp: <liuminzhao 08/08/2013 23:13:02>
-## bootstrap on tours data
-## weight2 and weight3
-## scaled by 1/100
-## covariates: 3 treatments and 2 races
-## 2013/07/29 add init1 and init9 , modfiied saved file name, change to 10 core
-## 2013/08/03 Using QRMissingBi.R and change boot to 50
-## 2013/08/08 on Int, age, Race
+##' Time-stamp: <liuminzhao 08/08/2013 23:13:05>
+##' bootstrap on tours data
+##' weight2 and weight3
+##' scaled by 1/100
+##' covariates: 3 treatments and 2 races
+##' 2013/07/07 MNAR
+##' 2013/07/14 y1 0.1 and y2 0.9 bootstrap CI behave weird,
+##' not include estimated statistics, thus track converge too.
+##' and only fit mod1 and mod9
+##' Age race, MNAR
+##' 2013/08/06 Using ToursMNAR3.R (uobyqa method and homo model)
+
 rm(list=ls())
 library(compiler)
 enableJIT(3)
@@ -14,7 +18,7 @@ enableJIT(3)
 library(rootSolve)
 library(quantreg)
 library(minqa)
-source('../code/QRMissingBi.R')
+source('~/Documents/qrmissing/code/ToursMNAR3.R')
 source('~/Documents/qrmissing/code/sendEmail.R')
 library(doMC)
 registerDoMC()
@@ -42,7 +46,7 @@ NEWTOURS <- data.frame(w2 = weight2, w3 = weight3, age_center, race3, w1 = weigh
 ###############
 ## BOOTSTRAP
 ###############
-boot <- 10
+boot <- 50
 n <- dim(NEWTOURS)[1]
 y <- matrix(0, n, 2)
 X <- matrix(0, n, 3)
@@ -50,7 +54,7 @@ X[,1] <- 1
 
 start <- proc.time()[3]
 
-QRMissingBic <- cmpfun(QRMissingBi)
+ToursMNARc <- cmpfun(ToursMNAR)
 
 result <- foreach(icount(boot), .combine = rbind) %dopar% {
   indices <- sample(n, replace = TRUE)
@@ -62,17 +66,18 @@ result <- foreach(icount(boot), .combine = rbind) %dopar% {
   R <- 1 - as.numeric(is.na(dat$w3))
   y[is.na(y[,2]),2] <- 0
 
-  mod1 <- QRMissingBic(y, R, X, tau = 0.1)
-  mod3 <- QRMissingBic(y, R, X, tau = 0.3)
-  mod5 <- QRMissingBic(y, R, X, tau = 0.5)
-  mod7 <- QRMissingBic(y, R, X, tau = 0.7)
-  mod9 <- QRMissingBic(y, R, X, tau = 0.9)
+  mod1 <- ToursMNARc(y, R, X, tau = 0.1)
+  mod3 <- ToursMNARc(y, R, X, tau = 0.3)
+  mod5 <- ToursMNARc(y, R, X, tau = 0.5)
+  mod7 <- ToursMNARc(y, R, X, tau = 0.7)
+  mod9 <- ToursMNARc(y, R, X, tau = 0.9)
 
   coef1 <- coef(mod1)
   coef3 <- coef(mod3)
   coef5 <- coef(mod5)
   coef7 <- coef(mod7)
   coef9 <- coef(mod9)
+
   count1 <- mod1$ierr
   count3 <- mod3$ierr
   count5 <- mod5$ierr
@@ -86,7 +91,7 @@ result <- foreach(icount(boot), .combine = rbind) %dopar% {
 
 }
 
-write.table(result, file = "toursbootagerace-0808.txt", row.names = FALSE, col.names = FALSE)
-sendEmail(subject="boot tours age race", text="done", address="liuminzhao@gmail.com")
+write.table(result, file = "toursbootageracemnar-0808.txt", row.names = FALSE, col.names = FALSE)
+sendEmail(subject="boot tours age race mnar", text="done", address="liuminzhao@gmail.com")
 
 print(proc.time()[3] - start)
