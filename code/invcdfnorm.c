e97da1e4ed70cb1ac342c928e19b889756114249
Minzhao Liu <liuminzhao@gmail.com>
2013-08-18 15:04:38 -0500

    add mydbeta in invcdfnorm.c

--- code/invcdfnorm.c
+++ code/invcdfnorm.c
@@ -27,11 +27,15 @@ double F77_SUB(dgamma2)(double *x, double *shape, double *scale, int *give_log)
 
 void F77_SUB(rndstart)(void) { GetRNGstate(); }
 
-void F77_SUB(rndend)(void) { PutRNGstate(); }     
+void F77_SUB(rndend)(void) { PutRNGstate(); }
 double F77_SUB(myrnorm)(double *mu, double *sigma) { return rnorm(*mu, *sigma) ;}
 double F77_SUB(myrunif)(double *a, double *b) { return runif(*a, *b) ;}
 
 double F77_SUB(myrbeta)(double *a, double *b) { return rbeta(*a, *b) ; }
+
+double F77_SUB(mydbeta)(double *x, double *a, double *b, int *give_log){
+  return dbeta(*x, *a, *b, *give_log);
+}
 
 int F77_SUB(ihmssf)(int *i, int *j, int *n){
   int ans;
@@ -41,15 +45,14 @@ int F77_SUB(ihmssf)(int *i, int *j, int *n){
   i1=*i-1;
   j1=*j-1;
 
-  if (i <= j) 
+  if (i <= j)
     {
       ans=*n*i1-*i*i1/2+*j ;
     }
-  else 
+  else
     {
       ans=*n*j1-*j*j1/2+*i;
     }
 
   return ans;
 }
-
