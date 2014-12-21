97b720b131e2748ef04cbff1982bb117e24f7557
Minzhao Liu <liuminzhao@gmail.com>
2014-12-21 12:09:06 -0600

    add header and initial xstarc2

--- code/Lipsitz.R
+++ code/Lipsitz.R
@@ -1,3 +1,14 @@
+
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param X [n, p] : same for same subject
+##' @param y [n, 2] : y[, 2] maybe missing
+##' @param R [n] indicator if it 2nd component is missing, 1 is not missing, 0 is missing
+##' @param tau quantile of interest
+##' @return
+##' @author Minzhao Liu
 Lipsitz <- function(X, y, R, tau) {
     n <- dim(y)[1]
     y1 <- y[, 1]
